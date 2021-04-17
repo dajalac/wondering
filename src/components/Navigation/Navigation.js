547e252d1b739e66cd5ab5dc771ec354e82610fd
danielle ajala <danielleajala@gmail.com>
2021-04-17 18:34:55 -0500

    logo added1

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,17 @@
 import React from 'react'; 
 import './Navigation.css';
+import logo from './thought-bubble.png';
 
 const Navigation = () => {
     return( 
         <nav className ='flex-container' >
-            <p className= ' f3 underline pointer pa3'>Logo</p>
-            <ul>
-                <li><p className= ' f3 underline pointer pa3'>Setting</p></li>
-                <li><p className= ' f3 underline pointer pa3'>Sigin out</p> </li>
+            <img className= 'pa3' alt='Logo' src ={logo} width='80' height='80'></img>
+            <ul className ='pa0'>
+                <li><p className= ' f3 underline pointer pa1'>Setting</p></li>
+                <li><p className= ' f3 underline pointer pa1'>Sigin out</p> </li>
                 
             </ul>
+           
               
     
         </nav>
