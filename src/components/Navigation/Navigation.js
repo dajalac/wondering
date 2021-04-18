104dae76173085c0a66d19fbdadd82f97bd0ea60
danielle ajala <danielleajala@gmail.com>
2021-04-17 20:50:39 -0500

    navbar + logo + background color

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,20 +4,19 @@ import logo from './thought-bubble.png';
 
 const Navigation = () => {
     return( 
-        <nav className ='flex-container' >
-            <img className= 'pa3' alt='Logo' src ={logo} width='80' height='80'></img>
-            <ul className ='pa0'>
-                <li><p className= ' f3 underline pointer pa1'>Setting</p></li>
-                <li><p className= ' f3 underline pointer pa1'>Sigin out</p> </li>
-                
-            </ul>
-           
-              
-    
-        </nav>
+       
+            <nav className=' main-nav' >
+                <img className=' pa2 item logoFormat' alt='Logo' src={logo} width='60' height='60'></img>
+                <div className='divider'></div>
+                <ul className='item'>
+                    <li className='f3  underline '><p>Setting</p> </li>
+                    <li className=' f3 underline pointer'><p>Sign out</p> </li>
+                </ul>
+
+            </nav>
+        
         
       
     )
 }
-
 export default Navigation; 
