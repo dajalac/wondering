aed16d50d0ab6666e01b35720abc6d37c3bd7e9c
danielle ajala <danielleajala@gmail.com>
2021-04-19 10:21:23 -0500

    clarifai implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import React, { useState , useEffect} from 'react';
 import './App.css';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Greeting from './components/Greeting/Greeting';
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkFrom'
+import PredictionRestults from './components/PredictionResults/PredictionResults';
 
+const app = new Clarifai.App({
+  apiKey: process.env.REACT_APP_CLARIFAI_KEY 
+
+});
 
 const particlesOptions = {
   particles: {
@@ -20,13 +27,36 @@ const particlesOptions = {
 }
 
 function App() {
+
+  // states
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  // user changes the URL
+  const onInputChange =(event) =>{
+    setInput (event.target.value);
+  }
+
+  const onButtonSubmit = ()=>{
+    setImageUrl(input);
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, input)
+      .then(
+        function(response){
+          console.log(response)
+        },
+        function(err){}
+      )
+
+  }
   return (
     <div className='App'>
       <Particles className='particles' params={particlesOptions}/>
       <Navigation/>
       <Greeting/>
       <Rank/>
-      <ImageLinkForm/>
+      <ImageLinkForm inputChange ={onInputChange} buttonSubmit ={onButtonSubmit}/>
+      <PredictionRestults imageUrl ={imageUrl}/>
     </div>
   );
 }
--- src/components/ImageLinkForm/ImageLinkFrom.js
+++ src/components/ImageLinkForm/ImageLinkFrom.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './ImageLinkForm.css'
 
-const ImageLinkForm = ()=>{
+const ImageLinkForm = ({inputChange, buttonSubmit })=>{
     return(
       <div>
           <p className = 'f3 center'>
               {'Past the URL of an image and see the magic!'}
           </p>
+          <div className='center'>
           <div className = 'form center pa4 br3 shadow-5'>
-              <input className='pa2 w-70 fa4' type ='text'></input>
-              <button className='w-30 grow f4 link ph3 pv2 dib white bg-green  '>Detect</button>
-              
+              <input className='pa2 w-70 fa4 center' type ='text' onChange={inputChange}></input>
+              <button className='w-30 grow f4 link ph3 pv2 dib white bg-green 'onClick={buttonSubmit}>Detect</button>
           </div>
+          </div>
+          
 
       </div>
     )
